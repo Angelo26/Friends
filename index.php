43acfc85d087f4ba028bbba4ceddafc520c0af93
Angelo26 <angelmaden333@gmail.com>
2022-05-17 00:53:44 +0545

    added session at top

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+
+<?php
+    session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,7 +14,7 @@
     <link rel="shortcut icon" type="image/png" href="pictures/couch.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins|Lobster Two|Oleo Script Swash Caps|Kaushan Script|Kalam|Abril Fatface|Alfa Slab One|Source Serif Pro|Shadows Into Ligh|Macondo|Pacifico|Dancing Script|Roboto|Lato|Josefin Sans|Send Flowers|Inconsolata">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins|Lobster Two|Permanent Marker|Architects Daughter|Oleo Script Swash Caps|Kaushan Script|Kalam|Abril Fatface|Alfa Slab One|Source Serif Pro|Shadows Into Light|Macondo|Pacifico|Dancing Script|Roboto|Lato|Josefin Sans|Send Flowers|Inconsolata">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     
@@ -26,7 +31,7 @@
                 <div class="log">
                     <h2>Join with your friends now!</h2>
                     
-                    <form action="signIn.php">
+                    <form action="includes/signin.inc.php" method = "POST">
                         <div class="form-group">
                             <div class="username">
                                 <label for="uname">Email or Username</label>
@@ -36,16 +41,16 @@
                                 <label for="lpwd">Password</label>
                                 <input type="password" name="lpwd" id="lpwd">
                             </div>
-                            <button type="submit">Sign In</button>
+                            <button name="login" type="submit">Sign In</button>
                             <p>New to <span id="regLogo">FRIENDS</span>?<span id="signup"> Create an account.</span></p>
                         </div>
                     </form>
                 </div>
 
                 <div class="reg">
-                    <h2>Create new account here!</h2>
+                    <h2>Lets get started</h2>
                     
-                    <form action="signUp.php">
+                    <form action="includes/signup.inc.php" method="POST">
                         <div class="form-group">
                   
                             <div class="email">
@@ -60,7 +65,7 @@
                                 <label for="crpwd">Confirm Password</label>
                                 <input type="password" name="crpwd" id="crpwd">
                             </div>
-                            <button type="submit">Sign Up</button>
+                            <button name="register" type="submit">Sign Up</button>
                             <p>Already have an account? <span id="signin">Sign in.</span></p>
                         </div>
                     </form>
