a90a44b11b0a4ea86784decffa838275da26f71a
Angelo26 <angelmaden333@gmail.com>
2022-05-16 22:54:21 +0545

    connected database

---
+++ includes/db.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+class DBconnection {
+  Protected function dbConnect(){
+    $this->servername = "localhost";
+    $this->dbname = "friends";
+    $this->username = "root";
+    $this->password = "";
+    
+    try {
+      $this->conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username, $this->password);
+      $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      echo "ok done";
+    } catch(PDOException $e) {
+      echo "Connection failed: " . $e->getMessage();
+    }
+  }
+  
+}
+// $db = new DBconnection();
+// $db->dbConnect();
+
+?>
