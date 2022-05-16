bf27bca7d2e9f4f2131950c52bd3e89022ec4cdc
Angelo26 <angelmaden333@gmail.com>
2022-05-17 00:51:28 +0545

    signup added and tested successful

---
+++ includes/signup.inc.php
@@ -0,0 +1,55 @@
+<?php
+if(isset($_POST['register'])) {
+	include 'db.inc.php';	
+
+    class Register extends DBconnection {
+      Protected $email;
+      Protected $password;
+      Protected $confirm_password;
+    
+      function __construct() {
+        $this->email = $_POST['email'];
+        $this->password = $_POST['rpwd'];
+        $this->confirm_password = $_POST['crpwd'];    
+      }
+
+      function addInfo() { 
+        $email = $this->email;
+        $pwd = $this->password;
+        $cpwd = $this->confirm_password;
+
+        if($pwd!==$cpwd){
+            header("Location: ../index.php?register=passwords donot match.");
+            exit();   
+        }
+        else{
+            try{
+                $sql = "SELECT * FROM users WHERE email=:email";
+                $stmt = $this->dbConnect()->prepare($sql);
+                $stmt->execute(['email'=>$email]);
+                $result = $stmt->rowCount();
+                if($result>0){
+                    header("Location: ../index.php?user already exist.");
+                    exit();
+                }
+                else{
+                    $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+                    $sql = "Insert into users(email, password) values(:email, :hashedPwd)";
+                    $stmt = $this->dbConnect()->prepare($sql);
+                    if($stmt->execute(['email'=>$email, 'hashedPwd'=>$hashedPwd])){
+                        header("Location: ../index.php?register=successful");
+                        exit();   
+                    }
+                }
+
+            }catch(PDOException $e){
+                echo "Insertion failed: " . $e->getMessage();
+            }
+        }
+      }
+    }
+    $userObj = new Register();
+    $userObj->addInfo();
+}
+
+?>
