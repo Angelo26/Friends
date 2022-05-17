8f38c957b53a2a4a895e4c601fffd06c6476d72b
Angelo26 <angelmaden333@gmail.com>
2022-05-17 11:27:42 +0545

    overwritten better connection code

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -1,23 +1,44 @@
 <?php
 
 class DBconnection {
+
   Protected function dbConnect(){
     $this->servername = "localhost";
     $this->dbname = "friends";
     $this->username = "root";
     $this->password = "";
-    
     try {
       $this->conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username, $this->password);
       $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      echo "ok done";
+      return $this->conn;
     } catch(PDOException $e) {
       echo "Connection failed: " . $e->getMessage();
     }
   }
-  
-}
-// $db = new DBconnection();
-// $db->dbConnect();
+  }
+//   class DBconnection{
+//     private $host = "localhost";
+//     private $nome_db = "friends";
+//     private $username = "root";
+//     private $password = "";
+//     public $conn;
 
+//     protected function dbConnect()
+//     {
+
+//         $this->conn = null;
+//         try
+//         {
+//             $this->conn = new PDO("mysql:host=".$this->host.";dbname=".$this->nome_db, $this->username,$this->password);
+//             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//             $this->conn->exec("SET NAMES utf8");
+
+//             return $this->conn;
+//         }
+//         catch(PDOException $e)
+//         {
+//           echo $e->getMessage();
+//         }
+//     }
+// }
 ?>
