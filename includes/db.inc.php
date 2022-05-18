8b38c4a5a4263c5bacd5c722f33fabef9a66ec0a
Angelo26 <angelmaden333@gmail.com>
2022-05-18 10:36:23 +0545

    fixed the db connection

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -14,31 +14,7 @@ class DBconnection {
     } catch(PDOException $e) {
       echo "Connection failed: " . $e->getMessage();
     }
-  }
-  }
-//   class DBconnection{
-//     private $host = "localhost";
-//     private $nome_db = "friends";
-//     private $username = "root";
-//     private $password = "";
-//     public $conn;
+  }  
+}
 
-//     protected function dbConnect()
-//     {
-
-//         $this->conn = null;
-//         try
-//         {
-//             $this->conn = new PDO("mysql:host=".$this->host.";dbname=".$this->nome_db, $this->username,$this->password);
-//             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//             $this->conn->exec("SET NAMES utf8");
-
-//             return $this->conn;
-//         }
-//         catch(PDOException $e)
-//         {
-//           echo $e->getMessage();
-//         }
-//     }
-// }
 ?>
