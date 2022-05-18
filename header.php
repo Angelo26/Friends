36e314bbce5121cee80d456595d007cf16bdc040
Angelo26 <angelmaden333@gmail.com>
2022-05-18 10:34:49 +0545

    added new google font

--- header.php
+++ header.php
@@ -10,10 +10,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/friends.css">
     <title>Friends</title>
-    <link rel="shortcut icon" type="image/png" href="pictures/couch.png">
+    <link rel="shortcut icon" type="image/png" href="pictures/peephole.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins|Lobster Two|Permanent Marker|Architects Daughter|Oleo Script Swash Caps|Kaushan Script|Kalam|Abril Fatface|Alfa Slab One|Source Serif Pro|Shadows Into Light|Macondo|Pacifico|Dancing Script|Roboto|Lato|Josefin Sans|Send Flowers|Inconsolata">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins|Lobster Two|Permanent Marker|Architects Daughter|Grape Nuts|Caveat|Oleo Script Swash Caps|Kaushan Script|Kalam|Abril Fatface|Alfa Slab One|Source Serif Pro|Shadows Into Light|Macondo|Pacifico|Dancing Script|Roboto|Lato|Josefin Sans|Send Flowers|Inconsolata">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     
